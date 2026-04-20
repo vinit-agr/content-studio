78a90aaf3532d993a5f765b95858b20c678ad7ee
Vinit Agrawal <vinit@hellotars.com>
2026-04-21 01:04:38 +0530

    feat(primitives): add barrel export

---
+++ tools/remotion/src/primitives/index.ts
@@ -0,0 +1,9 @@
+// tools/remotion/src/primitives/index.ts
+export { TitleCard, type TitleCardProps } from './TitleCard';
+export { Caption, type CaptionProps } from './Caption';
+export { Document, type DocumentProps } from './Document';
+export { Chunk, type ChunkProps } from './Chunk';
+export { Span, type SpanProps } from './Span';
+export { Token, type TokenProps } from './Token';
+export { MetricBar, type MetricBarProps } from './MetricBar';
+export { Cursor, type CursorProps } from './Cursor';
